92ee6878b16ec7fbfb0ca5f63b66c3e26000468a
SaeedPoureshghi <saeed.po@gmail.com>
2024-05-05 21:19:41 +0300

    Fix splash screen

--- src/screens/Splash.tsx
+++ src/screens/Splash.tsx
@@ -1,24 +1,32 @@
-import useUser from '../hooks/useUser'
-import WebApp from '@twa-dev/sdk'
-import {Navigate} from 'react-router-dom'
+import useUser from "../hooks/useUser";
+import WebApp from "@twa-dev/sdk";
+import { Navigate } from "react-router-dom";
+import logo from "../assets/logo.jpg";
+import { Spin, Typography } from "antd";
 
 const Splash = () => {
-    WebApp.ready();
-    WebApp.expand();
-    const initData = WebApp.initDataUnsafe;
-    
-    const {loading,profile,user} = useUser({initData})
+  WebApp.ready();
+  WebApp.expand();
+  const initData = WebApp.initDataUnsafe;
 
+  const { Text } = Typography;
 
-    if(loading){
-        return <div>Loading...</div>
-    }
-   
-    if(profile){
-        return <Navigate to="/home"/>
-    }
+  const { loading, profile, user } = useUser({ initData });
 
-    return <Navigate to="/create" state={{user}}/>
-}
+  if (loading) {
+    return (
+    <div className="splash">
+        <img width={"40px"} height={"40px"} style={{borderRadius:"50%"}} src={logo} alt="logo" />
+        <Text strong>Persian Connect</Text>
+        <Spin size="large"/>
+    </div>)
+  }
 
-export default Splash
+  if (profile) {
+    return <Navigate to="/home" />;
+  }
+
+  return <Navigate to="/create" state={{ user }} />;
+};
+
+export default Splash;
