546aa4d07d58e47f8f05f8994affad5a3233b859
SaeedPoureshghi <saeed.po@gmail.com>
2024-05-05 14:31:57 +0300

    [UPDATE] vite-config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import basicSsl from "@vitejs/plugin-basic-ssl";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // plugins: [react(), basicSsl()],
-  // build: {
-  //   outDir: "./docs",
-  // },
-  // base: "/client-date/",
+  // plugins: [react()],
+  plugins: [react(), basicSsl()],
+  build: {
+    outDir: "./docs",
+  },
+  base: "/client-date/",
 });
